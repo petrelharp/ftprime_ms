bb58dc7eead4a639b642ff2e570b815e4fae6201
Jaime Ashander <jashander@ucdavis.edu>
2017-11-04 15:57:03 -0700

    Add run check to the 'script'

--- sims/run_check.sh
+++ sims/run_check.sh
@@ -5,4 +5,5 @@ python ./benchmark-simuPOP.py --theta 10 --rho 10 --nsam 10 --pdel 0 --gc 50  \
 simupoptree.hdf5
 python ./benchmarking.py --theta 10 --rho 10 --nsam 10 --pdel 0 --gc 50 \
 	--outfile1 out.log.gz --seed 42 --popsize 100 --treefile fwdpptree.hdf5
+python ./check.py
 
