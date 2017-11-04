9e889c87eb33d388c5abc397a13a417072cfc939
Jaime Ashander <jashander@ucdavis.edu>
2017-11-04 16:12:17 -0700

    Change wget to right script from my repo :)

--- sims/run_check.sh
+++ sims/run_check.sh
@@ -1,5 +1,5 @@
 echo "Must have fwdpy11_arg_example installed!!"
-wget https://github.com/molpopgen/fwdpy11_arg_example/blob/update_to_edge/benchmarking.py
+wget https://github.com/ashander/fwdpy11_arg_example/blob/check-trees/benchmarking.py
 python ./benchmark-simuPOP.py --theta 10 --rho 10 --nsam 10 --pdel 0 --gc 50  \
 	--outfile1 out.log.gz --seed 42 --popsize 100 --treefile
 simupoptree.hdf5
