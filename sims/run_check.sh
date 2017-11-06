ba620eb4fb8707b25abf7b81b97ea1d96af5313c
Jaime Ashander <jashander@ucdavis.edu>
2017-11-06 09:36:53 -0800

    Formatting issue in run script

--- sims/run_check.sh
+++ sims/run_check.sh
@@ -1,9 +1,6 @@
 echo "Must have fwdpy11_arg_example installed!!"
 wget https://github.com/ashander/fwdpy11_arg_example/blob/check-trees/benchmarking.py
-python ./benchmark-simuPOP.py --theta 10 --rho 10 --nsam 10 --pdel 0 --gc 50  \
-	--outfile1 out.log.gz --seed 42 --popsize 100 --treefile
-simupoptree.hdf5
-python ./benchmarking.py --theta 10 --rho 10 --nsam 10 --pdel 0 --gc 50 \
-	--outfile1 out.log.gz --seed 42 --popsize 100 --treefile fwdpptree.hdf5
+python ./benchmark-simuPOP.py --theta 10 --rho 10 --nsam 10 --pdel 0 --gc 50  --outfile1 out.log.gz --seed 42 --popsize 100 --treefile simupoptree.hdf5
+python ./benchmarking.py --theta 10 --rho 10 --nsam 10 --pdel 0 --gc 50 --outfile1 out.log.gz --seed 42 --popsize 100 --treefile fwdpptree.hdf5
 python ./check.py
 
