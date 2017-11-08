1d62e6f8b9aa8b7263e151cf3ac4a508ca7cb26b
Jaime Ashander <jashander@ucdavis.edu>
2017-11-07 16:21:49 -0800

    Correct scaling, run checks

--- sims/check.py
+++ sims/check.py
@@ -1,54 +1,60 @@
 # coding: utf-8
 import msprime
 import random
+import argparse
 from matplotlib import pyplot as plt
 
 
-random.seed(111)
+parser = argparse.ArgumentParser(description='small test script')
+parser.add_argument("--popsize","-N", type=int, dest="popsize",
+        help="size of each subpopulation",default=100)
+parser.add_argument("--theta","-ϴ", type=int, dest="theta",
+                    help="total mutation rate for whole chromosome: theta = 4 N u/site (sites)", default=100)
+parser.add_argument("--rho","-ρ", type=int, dest="rho",
+                    help="total recombination rate for whole chromosome: rho = 4 N r/site (sites)", default=100)
+parser.add_argument("--nsam","-k", type=int, dest="nsam",
+                    help="total number of samples with msprime", default=10)
+parser.add_argument("--sims","-n", type=int, dest="NUM",
+                    help="total number of sims with msprime", default=100)
+parser.add_argument("--seed","-s", type=int, dest="seed",
+                    help="random seed")
+parser.add_argument("--out-fig","-o", type=str, dest="figname",
+                    help="desination, e.g. check-results.png")
+args = parser.parse_args()
+
+random.seed(args.seed)
 ft_ts = msprime.load('simupoptree.hdf5')
 L = ft_ts.sequence_length
-fwd_ts = msprime.load('fwdpptree.hdf5')
+
 L = ft_ts.sequence_length
-#assert L == fwd_ts.sequence_length, "fwdpp {} and simupop {} don't match".format(fwd_ts.sequence_length, L)
-NUM = 100
-
-tss = {'simupop': ft_ts, 'fwdpp':  fwd_ts}
-
-L = fwd_ts.sequence_length
-theta = 10.
-rho = 10.
-mut_rate = theta/4./ L
-rec_rate = rho/4. / L
+theta = args.theta
+rho = args.rho
+Ne = args.popsize
+mut_rate = theta/4./ L /Ne
+rec_rate = rho/4. / L /Ne
 S_msprime = []
 
-for _ in range(NUM):
-    g = msprime.simulate(10, Ne=100, length=L, mutation_rate=mut_rate,
+for _ in range(args.NUM):
+    g = msprime.simulate(args.nsam, Ne=Ne, length=L, mutation_rate=mut_rate,
                          recombination_rate=rec_rate)
     S_msprime.append(len([i for i in g.variants()]))
 
-k ='fwdpp'
-ts = fwd_ts
-
-# for k, ts in tss.items():
 rng = msprime.RandomGenerator(random.randint(1, 2**32 - 1))
 nodes = msprime.NodeTable()
 edges = msprime.EdgeTable()
 sites = msprime.SiteTable()
 mutations = msprime.MutationTable()
-ts.dump_tables(nodes=nodes, edges=edges, sites=sites, mutations=mutations)
+ft_ts.dump_tables(nodes=nodes, edges=edges, sites=sites, mutations=mutations)
 mutgen = msprime.MutationGenerator(rng, mut_rate)
 mutgen.generate(nodes, edges, sites, mutations)
-ts = msprime.load_tables(nodes=nodes, edges=edges, sites=sites,
+ft_ts = msprime.load_tables(nodes=nodes, edges=edges, sites=sites,
                          mutations=mutations)
-tss[k] = ts
-
-
 
 
 
 tmp = plt.hist(S_msprime, color='orange', normed=1)
 plt.xlabel('Number variants')
-plt.title(' '.join([str(NUM), 'msprime sim and 1', 'fwdpp', 'sim']))
+plt.title(' '.join([str(args.NUM), 'msprime sim and 1', 'ftprime', 'sim']))
 #for k, ts in tss.items():
-plt.axvline(x=len([i for i in ts.variants()]))
-plt.savefig('check-results.png')
+plt.axvline(x=len([i for i in ft_ts.variants()]))
+plt.savefig(args.figname)
--- sims/run_check.sh
+++ sims/run_check.sh
@@ -1,6 +1,8 @@
-echo "Must have fwdpy11_arg_example installed!!"
-wget https://github.com/ashander/fwdpy11_arg_example/blob/check-trees/benchmarking.py
-python ./benchmark-simuPOP.py --theta 10 --rho 10 --nsam 10 --pdel 0 --gc 50  --outfile1 out.log.gz --seed 42 --popsize 100 --treefile simupoptree.hdf5
-python ./benchmarking.py --theta 10 --rho 10 --nsam 10 --pdel 0 --gc 50 --outfile1 out.log.gz --seed 42 --popsize 100 --treefile fwdpptree.hdf5
-python ./check.py
+python ./benchmark-simuPOP.py --theta 10 --rho 10 --nsam 10 --pdel 0 --gc 50 --outfile1 /dev/null --seed 42 --popsize 100 --treefile simupoptree.hdf5
+python ./check.py --rho 10 --theta 10 --popsize 100 --seed 111 -o check-10-10-100.png
 
+python ./benchmark-simuPOP.py --theta 5 --rho 5 --nsam 10 --pdel 0 --gc 50 --outfile1 /dev/null --seed 42 --popsize 200 --treefile simupoptree.hdf5
+python ./check.py --rho 5 --theta 5 --popsize 200 --seed 111 -o check-5-5-200.png
+
+python ./benchmark-simuPOP.py --theta 20 --rho 20 --nsam 10 --pdel 0 --gc 50 --outfile1 /dev/null --seed 42 --popsize 100 --treefile simupoptree.hdf5
+python ./check.py --rho 20 --theta 20 --popsize 100 --seed 111 -o check-20-20-100.png -N 500
