22fa5a639205620270ddc8cf150222c18c2afe00
Jaime Ashander <jashander@ucdavis.edu>
2017-11-27 15:17:29 -0800

    Cleanup

--- sims/big_test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python3 benchmark-simuPOP.py --seed 42 --nsam 10 --pdel 0.1 --theta 100 --gc 100 --rho 100 -N 1000 --outfile1 out.csv.gz
--- sims/test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python3 benchmark-simuPOP.py --seed 43 --nsam 10 --pdel 0.1 --theta 1 --rho 1 -N 100 --outfile1 out.csv.gz
