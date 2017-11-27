3831c59b86f928733f16ee563c62569514784ba7
molpopgen <krthornt@uci.edu>
2017-11-27 14:07:22 -0800

    exploratory plot of memory use

--- sims/quick_plot.py
+++ sims/quick_plot.py
@@ -49,3 +49,36 @@ ax_simupop.set_xticks([1e3,1e4,1e5])
 # plt.legend(bbox_to_anchor=(1.05, 1), loc=2)
 plt.tight_layout()
 plt.savefig("quickfig.png")
+
+fig, (ax_fwdpp, ax_fwdpp_arg, ax_simupop) = plt.subplots(
+    3, sharex=True, sharey=True)
+# ax.margins(0.05) # Optional, just adds 5% padding to the autoscaling
+for name, group in groups:
+    lstyle = 'solid'
+    ax = ax_fwdpp
+    if name[0] == 'fwdpy11' and name[1] is True:
+        ax = ax_fwdpp_arg
+    m = 'o'
+    if name[0] == 'simuPOP':
+        ax = ax_simupop
+    mfacecolor = colors[name[3]]
+    if name[2] is True:
+        lstyle = 'dashed'
+        mfacecolor = 'none'
+    popsize = int(name[3])
+    ax.plot(group.rho, group.mem/(1024.**2.), marker=m,
+            ms=6, linestyle=lstyle, label=r'$N = {}$'.format(popsize),
+            color=colors[name[3]],
+            markerfacecolor=mfacecolor)
+
+ax_fwdpp.legend(loc='best')
+ax_fwdpp.set_title("fwdpy11 with neutral mutations")
+ax_fwdpp_arg.set_title("fwdpy11 with ancestry tracking")
+ax_fwdpp_arg.set_ylabel("Peak RAM use (Gigabytes)")
+ax_simupop.set_title("simuPOP with ancestry tracking")
+ax_simupop.set_xlabel('Scaled recmbination rate (' + r'$\rho = 4Nr$)')
+ax_simupop.set_xticks([1e3,1e4,1e5])
+# ax.set_yscale("log")
+# plt.legend(bbox_to_anchor=(1.05, 1), loc=2)
+plt.tight_layout()
+plt.savefig("quickfig_mem.png")
