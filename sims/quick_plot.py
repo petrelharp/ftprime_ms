b07c6aeb8ffecd7232e98bdd82d42260948c70ab
molpopgen <krthornt@uci.edu>
2017-12-21 12:57:14 -0800

    start plotting code for sims w/o selection

--- sims/quick_plot.py
+++ sims/quick_plot.py
@@ -20,7 +20,7 @@ for x in sorted(data.N.unique()):
     i = i + 1
 
 
-data_neut = data[data['sel'] == False]
+data_neut = data[data['sel'] == False].copy(deep=True)
 
 # Plot out the run times for the simulations with selection
 data = data[data['sel'] == True]
@@ -109,3 +109,53 @@ ax_simupop.set_xscale('log')
 ax_fwdpp.legend(loc='best')
 fig.tight_layout()
 plt.savefig("speedup.pdf")
+
+# Now, plots for sims w/o selection
+data_neut.sort_values(by='rho',inplace=True)
+groups = data_neut.groupby(['engine', 'arg', 'queue', 'N'])
+fig, ((ax_fwdpp, ax_fwdpp_arg), (ax_simupop, ax_simupop_arg)) = plt.subplots(
+    2, 2, sharex=True, sharey=True)
+for name, group in groups:
+    lstyle = 'solid'
+    if name[0] == 'fwdpy11' and name[1] is False:
+        ax = ax_fwdpp
+    if name[0] == 'fwdpy11' and name[1] is True:
+        print(group)
+        ax = ax_fwdpp_arg
+    m = 'o'
+    if name[0] == 'simuPOP':
+        ax = ax_simupop
+        if name[1] is True:
+            ax = ax_simupop_arg
+    mfacecolor = colors[name[3]]
+    if name[2] is True:
+        lstyle = 'dashed'
+        mfacecolor = 'none'
+    popsize = int(name[3])
+    ax.plot(group.rho, group.time / (60.**2.), marker=m,
+            ms=4, linestyle=lstyle, label=r'$N = {:.0e}$'.format(popsize),
+            color=colors[name[3]],
+            markerfacecolor=mfacecolor)
+
+ax_fwdpp.legend(loc='best')
+ax_fwdpp.set_title("fwdpy11 with neutral mutations",fontsize='medium')
+ax_fwdpp_arg.set_title("fwdpy11 with ancestry tracking",fontsize='medium')
+ax_simupop.set_ylabel("Run time (hours)")
+ax_fwdpp.set_ylabel("Run time (hours)")
+ax_simupop.set_title("simuPOP with neutral mutations",fontsize='medium')
+ax_simupop_arg.set_title("simuPOP with ancestry tracking",fontsize='medium')
+ax_simupop.set_xlabel('Scaled recombination rate (' + r'$\rho = 4Nr$)')
+ax_simupop_arg.set_xlabel('Scaled recombination rate (' + r'$\rho = 4Nr$)')
+ax_simupop_arg.set_xticks([1e3, 1e4, 1e5])
+for ax in (ax_simupop, ax_simupop_arg):
+    ax.set_xscale('log')
+#     for tick in ax.get_xticklabels():
+#         tick.set_rotation(45)
+    # ax.ticklabel_format(style='sci', axis='x', scilimits=(-3,0))
+# ax.set_yscale("log")
+# plt.legend(bbox_to_anchor=(1.05, 1), loc=2)
+# plt.xlabel('Scaled recombination rate (' + r'$\rho = 4Nr$)')
+fig.tight_layout()
+# plt.ticklabel_format(style='sci', axis='x', scilimits=(0,2))
+plt.savefig("rawspeed_nosel.pdf")
+
