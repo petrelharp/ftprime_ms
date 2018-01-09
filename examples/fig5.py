e3aece08fada7061fa1580028bc9f9c6c821f003
Jaime Ashander <jashander@ucdavis.edu>
2018-01-09 10:29:44 -0800

    Add output from ts.simplify to compare

--- examples/fig5.py
+++ examples/fig5.py
@@ -10,7 +10,21 @@ print("Full trees")
 for t in ts.trees():
     print(t.draw(format='unicode', node_label_text=full_node_map))
 
-print("Simplifed trees")
-tss = msprime.load_text(edges=open('edges.txt'), nodes=open('nodes.txt'))
+
+print("Simplifed trees with to J (9), K (10) with `ts.simplify()`)")
+tss = ts.simplify(samples=[9, 10])
 for t in tss.trees():
     print(t.draw(format='unicode'))
+
+
+print("Simplifed trees from tables in fig 5C",
+      "with J (0) and K(1) marked as samples.")
+tss2 = msprime.load_text(edges=open('edges.txt'), nodes=open('nodes.txt'))
+for t in tss2.trees():
+    print(t.draw(format='unicode'))
+
+print("note the nodes.flags differ between the output of `ts.simplify()`:")
+print(tss.dump_tables())
+print("\n\n\n...and the the trees loaded from the raw table:")
+print(tss2.dump_tables())
+
